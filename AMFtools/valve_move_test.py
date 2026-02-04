e99477d161e982b41bae782fb7377991da988ef3
smgrady <smgrady@gmail.com>
2026-02-04 16:48:04 -0600

    a whole bunch of testing!

--- AMFtools/valve_move_test.py
+++ AMFtools/valve_move_test.py
@@ -10,6 +10,36 @@
 # License : 
 #*******************************************************************************
 
+from amfValveControl import amfValveControl
+vc = amfValveControl()
+
+vc.setValvePort('A', 1)
+vc.setValvePort('B', 4)
+vc.setValvePort('C', 1)
+vc.setValvePort('D', 1)
+vc.setValvePort('E', 1)
+vc.setValvePort('F', 1)
+vc.getAllValves()
+
+vc.setValvePort('A', 1)
+vc.setValvePort('B', 2)
+vc.setValvePort('C', 1)
+vc.setValvePort('D', 1)
+vc.setValvePort('E', 1)
+vc.setValvePort('F', 1)
+vc.getAllValves()
+
+vc.setValvePort('A', 2)
+vc.setValvePort('B', 6)
+vc.setValvePort('C', 4)
+vc.setValvePort('D', 4)
+vc.setValvePort('E', 4)
+vc.setValvePort('F', 4)
+vc.getAllValves()
+
+
+
+
 import amfTools
 import time
 
@@ -35,6 +65,52 @@ for iValve, dInfo in enumerate(listAmf):
         time.sleep(0.1) # TODO - how low can this be?
         thisValve.disconnect()
         
+        
+        
+        
+from amfValveControl import amfValveControl
+vc = amfValveControl()
+
+vc.getAllValves()
+vc.setValveHome('A')
+
+
+vc.getValvePort('A')
+vc.setValvePort('A', 2)
+vc.getValvePort('A')
+
+
+vc.getValvePort('B')
+vc.setValvePort('B', 2)
+vc.getValvePort('B')
+
+vc.getValvePort('C')
+vc.setValvePort('C', 2)
+vc.getValvePort('C')
+
+vc.getValvePort('D')
+vc.setValvePort('D', 2)
+vc.getValvePort('D')
+
+vc.getValvePort('E')
+vc.setValvePort('E', 2)
+vc.getValvePort('E')
+
+
+vc.getAllValves()
+
+
+
+
+
+
+
+
+
+vc.valves
+raw_list = vc.getValveList()
+vc.setAllValvesHome()
+
 
 # Valve, serial, COM port (for now), index, 
 # A, P201-O00005087, COM6, 2, 
@@ -45,8 +121,8 @@ for iValve, dInfo in enumerate(listAmf):
 # F, P201-O00005330, COM7, 3
 # I learned another weird thing: if you send a .getValvePosition() it will send the state of the valve back before a disconnection, which means if you check the position, then move it, then check the position again, it will report the port information pre-move twice, and only when you send a separate port location request (after the disconnect) will you get the new, updated port....
 # valves start at 0 (0:5), ports start at 1 (1:nPorts)
-# 
-
+# because the .getValvePosition() quirk above we should double check "home" routing state for the side-by-side valves.
+# I think com port might change with reboot, or certainly if anything gets unplugged, so we need to map serial to valve letter
 
 # testing: set the first valve to port 1
 iValve = 0
