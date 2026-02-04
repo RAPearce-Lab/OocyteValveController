04d4ab000562505b7b5d0e6d3720a5f21938a0f9
smgrady <smgrady@gmail.com>
2026-02-04 16:52:25 -0600

    Created a class to handle all the hardware interface and wrap the original serial commands. Goes through a whole initialization and error checking, 1 get the list of connected valves 2. load a valve configuration (serial# to letter association) 3. be sure all valves are present 4. associate the letters with the serial number and object 5. home all valves and also has the static methods: getValveList setValveHome setAllValvesHome setValvePort getValvePort getAllValves

---
+++ amfValveControl.py
@@ -0,0 +1,72 @@
+import amfTools
+import json
+import os
+import time
+
+class amfValveControl:
+    def __init__(self):
+        configFile = "valve_config.json" 
+        # 1 get the list of connected valves
+        self.valveList = self.getValveList()
+        print("1. Connected valves discovered.")
+        # 2. load a valve configuration (serial# to letter association)
+        self.serialMap = {}
+        self.loadConfig(configFile)
+        print("2. Valve map loaded.")
+        # 3. be sure all valves are present
+        theseSerials = {v.serialnumber for v in self.valveList}
+        expectedSerials = set(self.serialMap.values())
+        if theseSerials != expectedSerials:
+            missingValves = expectedSerials - theseSerials
+            raise RuntimeError(f"unable to find valve: {missingValves}")
+        print("3. All valves confirmed.")
+        # 4. associate the letters with the serial number and object
+        self.initializeValves(self.valveList)
+        print("4. Letter mapping to hardware completed.")
+        self.setAllValvesHome()
+        print("5. All valves homed.")
+    def loadConfig(self,configFile):
+        thisFolder = os.path.dirname(__file__)
+        configFilePath = os.path.join(thisFolder,configFile)
+        try:
+            with open(configFilePath, 'r') as f:
+                self.serialMap = json.load(f)
+        except Exception as e:
+            print(f"I couldn't load {e}")
+    def initializeValves(self, valveList):
+        self.valves = {}
+        for label, sn in self.serialMap.items():
+            for hardware in valveList:
+                if hardware.serialnumber == sn:
+                    self.valves[label] = hardware
+        if len(self.valves) != len(self.serialMap):
+            raise RuntimeError("unable to initialize valves!")
+
+    @staticmethod
+    def getValveList():
+        return amfTools.util.getProductList("USB")
+    def setValveHome(self, valveID):
+        thisValve = amfTools.AMF(self.valves.get(valveID))
+        if not thisValve.getHomeStatus():
+            thisValve.home()
+            time.sleep(0.01) # 10ms is usually a very safe minimum (according to google. test?) 
+        thisValve.disconnect()
+    def setAllValvesHome(self):
+        for label in self.valves:
+            print(f"Homing Valve {label}.")
+            self.setValveHome(label)
+    def setValvePort(self, valveID, portID):
+        thisValve = amfTools.AMF(self.valves.get(valveID))
+        thisValve.valveShortestPath(portID, block= False)  # Non blocking function
+        time.sleep(0.01)
+        thisValve.disconnect()
+    def getValvePort(self, valveID):
+        thisValve = amfTools.AMF(self.valves.get(valveID))
+        portOut = thisValve.getValvePosition()
+        time.sleep(0.01)
+        thisValve.disconnect()
+        return portOut
+    def getAllValves(self):
+        for label in self.valves:
+            print(f"Valve: {label} at port {self.getValvePort(label)}.")
+
